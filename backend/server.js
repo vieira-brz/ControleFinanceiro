7793438e04e9e761f02d32b52b681c61b66770ac
vieira-brz <vieirabraz2003@gmail.com>
2024-02-16 19:49:59 -0300

    Correção conexão mongodb

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/financas', financas)
 
 
 // Iniciar servidor
-const MONGODB_URI = process.env.DB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
