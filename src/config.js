0fbc967d31b87c38ccb593d1c7899115b3f1ab51
vieira-brz <vieirabraz2003@gmail.com>
2024-02-21 15:28:46 -0300

    fix: Return Proxy to Live

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:9001'
-// export const API_URL = 'https://api-financas-beige.vercel.app'
+// export const API_URL = 'http://localhost:9001'
+export const API_URL = 'https://api-financas-beige.vercel.app'
