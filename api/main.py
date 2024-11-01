9466154351fcdf617ef8d7d82bdddbba4971f6f8
vieira-brz <vieirabraz2003@gmail.com>
2024-11-01 19:57:39 -0300

    API Atualização

--- api/main.py
+++ api/main.py
@@ -15,19 +15,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Função para adicionar cabeçalhos CORS
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"  # Permite todas as origens
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"  # Métodos permitidos
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"  # Cabeçalhos permitidos
-    return response
-
-# Configuração de CORS
-origins = [
-    "https://whitex-financeiro-web.vercel.app",  # Adicione aqui seu domínio frontend
-    "http://localhost:3000",  # Adicione aqui seu domínio local, se necessário
-]
-
 # Configuração do CORS
 app.add_middleware(
     CORSMiddleware,
@@ -40,93 +27,76 @@ app.add_middleware(
 # Teste de funcionamento da API
 @app.get('/')
 def log_working():
-    response = { "data": "Working..." }
-    return add_cors_headers(response)
+    return { "data": "Working..." }
 
 # Rotas para Categoria
 @app.post("/categorias/", response_model=schemas.Categoria)
 def create_categoria(categoria: schemas.CategoriaCreate, db: Session = Depends(get_db)):
-    response = crud.create_categoria(db=db, categoria=categoria)
-    return add_cors_headers(response)
+    return crud.create_categoria(db=db, categoria=categoria)
 
 @app.get("/categorias/", response_model=List[schemas.Categoria])
 def read_categorias(db: Session = Depends(get_db)):
-    response = crud.get_categorias(db)
-    return add_cors_headers(response)
+    return crud.get_categorias(db)
 
 @app.put("/categorias/{categoria_id}", response_model=schemas.Categoria)
 def update_categoria(categoria_id: int, categoria: schemas.CategoriaUpdate, db: Session = Depends(get_db)):
-    response = crud.update_categoria(db=db, categoria_id=categoria_id, categoria=categoria)
-    return add_cors_headers(response)
+    return crud.update_categoria(db=db, categoria_id=categoria_id, categoria=categoria)
 
 @app.delete("/categorias/{categoria_id}")
 def delete_categoria(categoria_id: int, db: Session = Depends(get_db)):
     crud.delete_categoria(db, categoria_id=categoria_id)
-    response = {"message": "Categoria deletada com sucesso"}
-    return add_cors_headers(response)
+    return {"message": "Categoria deletada com sucesso"}
 
 # Rotas para Transacao
 @app.post("/transacoes/", response_model=schemas.Transacao)
 def create_transacao(transacao: schemas.TransacaoCreate, db: Session = Depends(get_db)):
-    response = crud.create_transacao(db=db, transacao=transacao)
-    return add_cors_headers(response)
+    return crud.create_transacao(db=db, transacao=transacao)
 
 @app.get("/transacoes/", response_model=List[schemas.Transacao])
 def read_transacoes(db: Session = Depends(get_db)):
-    response = crud.get_transacoes(db)
-    return add_cors_headers(response)
+    return crud.get_transacoes(db)
 
 @app.put("/transacoes/{transacao_id}", response_model=schemas.Transacao)
 def update_transacao(transacao_id: int, transacao: schemas.TransacaoUpdate, db: Session = Depends(get_db)):
-    response = crud.update_transacao(db=db, transacao_id=transacao_id, transacao=transacao)
-    return add_cors_headers(response)
+    return crud.update_transacao(db=db, transacao_id=transacao_id, transacao=transacao)
 
 @app.delete("/transacoes/{transacao_id}")
 def delete_transacao(transacao_id: int, db: Session = Depends(get_db)):
     crud.delete_transacao(db, transacao_id=transacao_id)
-    response = {"message": "Transação deletada com sucesso"}
-    return add_cors_headers(response)
+    return {"message": "Transação deletada com sucesso"}
 
 # Rotas para Fatura
 @app.post("/faturas/", response_model=schemas.Fatura)
 def create_fatura(fatura: schemas.FaturaCreate, db: Session = Depends(get_db)):
-    response = crud.create_fatura(db=db, fatura=fatura)
-    return add_cors_headers(response)
+    return crud.create_fatura(db=db, fatura=fatura)
 
 @app.get("/faturas/", response_model=List[schemas.Fatura])
 def read_faturas(db: Session = Depends(get_db)):
-    response = crud.get_faturas(db)
-    return add_cors_headers(response)
+    return crud.get_faturas(db)
 
 @app.put("/faturas/{fatura_id}", response_model=schemas.Fatura)
 def update_fatura(fatura_id: int, fatura: schemas.FaturaUpdate, db: Session = Depends(get_db)):
-    response = crud.update_fatura(db=db, fatura_id=fatura_id, fatura=fatura)
-    return add_cors_headers(response)
+    return crud.update_fatura(db=db, fatura_id=fatura_id, fatura=fatura)
 
 @app.delete("/faturas/{fatura_id}")
 def delete_fatura(fatura_id: int, db: Session = Depends(get_db)):
     crud.delete_fatura(db, fatura_id=fatura_id)
-    response = {"message": "Fatura deletada com sucesso"}
-    return add_cors_headers(response)
+    return {"message": "Fatura deletada com sucesso"}
 
 # Rotas para Parcela
 @app.post("/parcelas/", response_model=schemas.Parcela)
 def create_parcela(parcela: schemas.ParcelaCreate, db: Session = Depends(get_db)):
-    response = crud.create_parcela(db=db, parcela=parcela)
-    return add_cors_headers(response)
+    return crud.create_parcela(db=db, parcela=parcela)
 
 @app.get("/parcelas/", response_model=List[schemas.Parcela])
 def read_parcelas(db: Session = Depends(get_db)):
-    response = crud.get_parcelas(db)
-    return add_cors_headers(response)
+    return crud.get_parcelas(db)
 
 @app.put("/parcelas/{parcela_id}", response_model=schemas.Parcela)
 def update_parcela(parcela_id: int, parcela: schemas.ParcelaUpdate, db: Session = Depends(get_db)):
-    response = crud.update_parcela(db=db, parcela_id=parcela_id, parcela=parcela)
-    return add_cors_headers(response)
+    return crud.update_parcela(db=db, parcela_id=parcela_id, parcela=parcela)
 
 @app.delete("/parcelas/{parcela_id}")
 def delete_parcela(parcela_id: int, db: Session = Depends(get_db)):
     crud.delete_parcela(db, parcela_id=parcela_id)
-    response = {"message": "Parcela deletada com sucesso"}
-    return add_cors_headers(response)
+    return {"message": "Parcela deletada com sucesso"}
