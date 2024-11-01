b6c03b1028f507039d52ada577992acb138ce22b
vieira-brz <vieirabraz2003@gmail.com>
2024-11-01 14:00:48 -0300

    Config JS

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,12 +1,3 @@
-// config.js
-import dotenv from 'dotenv';
-
-dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
-
-const API_BASE_URL = process.env.API_BASE_URL; // Acessa a variável de ambiente
-
-if (!API_BASE_URL) {
-    throw new Error("API_BASE_URL não está definida no arquivo .env");
-}
+const API_BASE_URL = "https://api-web-financeiro.vercel.app"; // Substitua pela URL da API em produção, se necessário
 
 export { API_BASE_URL };
