437a040ac2f826bc86fb4eae8cb0578b5c38f9e3
vieira-brz <vieirabraz2003@gmail.com>
2024-11-01 13:41:00 -0300

    Verificação ENV

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+if (!API_BASE_URL) {
+    console.error("A variável de ambiente VITE_API_BASE_URL não está definida.");
+}
+
 export { API_BASE_URL };
